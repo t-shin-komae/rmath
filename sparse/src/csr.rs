1c6bc4385596d3f5f27bc5b07ff1923feefb8919
shinichi <t-shin-komae>
2019-11-28 11:53:05 +0900

    Implement two constructors to a Csr matrix

--- sparse/src/csr.rs
+++ sparse/src/csr.rs
@@ -124,6 +124,64 @@ impl<T:Copy+Zero+Add<Output=T>+Sub<Output=T>+Neg<Output=T>> PartialEq for CsrVec
         self.sub(other).data.iter().all(|(_,val)| val.is_zero())
     }
 }
+
+/// Example
+/// [
+///     [0,0,1,0,0],
+///     [3,0,1,0,0],
+///     [0,4,0,1,0],
+///     [0,0,0,0,0],
+///     [0,0,0,0,5],
+/// ]
+/// 
+/// then
+///
+/// size:(5,5)
+/// data: [1,3,1,4,1,5]
+/// i_start: [0,1,1,2,2,4] --> [0,1,3,5,5]
+/// j_index: [2,0,2,1,3,4]
+
+#[derive(Debug)]
+pub struct Csr<T> {
+    size:(usize,usize),
+    data:Vec<T>,
+    i_start_index:Vec<usize>,
+    j_index:Vec<usize>,
+}
+
+use crate::Dok;
+use crate::coo::{Coo,Coordinate};
+impl<T:Zero+Copy> Csr<T> {
+    pub fn from_dok(src:Dok<T>)->Self{
+        Self::from_coo(Coo::from_dok(src))
+    }
+    pub fn from_coo(src:Coo<T>)-> Self{
+        let mut new_data = Vec::new();
+        let mut new_istart_index = Vec::new();
+        let mut new_j_index = Vec::new();
+        let mut last_row_number=0;
+        new_istart_index.push(0);
+        for (i,(coordinate,data)) in src.data.into_iter().enumerate(){
+            new_data.push(data);
+            new_j_index.push(coordinate.j);
+            if last_row_number != coordinate.i {
+                for _ in 0..(coordinate.i - last_row_number) {
+                    new_istart_index.push(i);
+                }
+                last_row_number = coordinate.i;
+            }
+        }
+
+        Self{
+            size:src.size,
+            data:new_data,
+            i_start_index:new_istart_index,
+            j_index:new_j_index,
+        }
+    }
+}
+
+
 #[cfg(test)]
 mod tests{
     #[test]
@@ -133,36 +191,42 @@ mod tests{
         let y = CsrVec::from_vec(&vec![0.,0.,3.,0.,4.,0.,0.,0.]);
         assert_eq!(CsrVec::from_vec(&vec![0.,0.,3.,3.,8.,0.,0.,0.]),x.add(&y));
         assert_eq!(CsrVec::from_vec(&vec![0.,0.,-3.,3.,0.,0.,0.,0.]),x.sub(&y));
-        // assert_eq!(4.*4.,x.dot(&y));
-        // assert_eq!(0.,x.get(0));
-        // assert_eq!(3.,x.get(3));
+        
+    }
+    #[test]
+    fn csr_operation() {
+        /// Example
+        /// x = 
+        /// [
+        ///     [0,0,1,0,0],
+        ///     [3,0,1,0,0],
+        ///     [0,4,0,1,0],
+        ///     [0,0,0,0,0],
+        ///     [0,0,0,0,5],
+        /// ]
+        /// 
+        /// then
+        ///
+        /// size:(5,5)
+        /// data: [1,3,1,4,1,5]
+        /// i_start: [0,1,1,2,2,4] --> [0,1,3,5,5]
+        /// j_index: [2,0,2,1,3,4]
+        ///
+        /// y = 
+        /// [
+        ///     [0,0,0,3,1]
+        ///     [0,0,0,0,0]
+        ///     [0,0,0,0,0]
+        ///     [0,0,0,3,0]
+        ///     [0,0,0,0,1]
+        /// ]
+        use super::*;
+        use crate::coo::Coo;
+        let x  = Csr::from_coo(Coo::from_array(&[(0,2,1.),(1,0,3.),(1,2,1.),(2,1,4.),(2,3,1.),(4,4,5.)],(5,5)));
+        let y  = Csr::from_coo(Coo::from_array(&[(0,3,3.),(0,4,1.),(3,3,3.),(4,4,1.)],(5,5)));
+        assert_eq!(vec![1.,3.,1.,4.,1.,5.],x.data);
+        assert_eq!(vec![0,1,3,5,5],x.i_start_index);
+        assert_eq!(vec![2,0,2,1,3,4],x.j_index);
+        
     }
 }
-
-pub struct Csr<T> {
-    size:(usize,usize),
-    data:Vec<T>,
-    i_start_index:Vec<usize>,
-    j_index:Vec<usize>,
-}
-
-// impl<T:Zero+Copy> Csr<T> {
-//     pub fn from_vec(src:&[&[T]])->Self{
-//         let data:Vec<(usize,T)>  = src.iter().enumerate().filter(|(_,val)| !val.is_zero()).map(|(i,&val)| (i,val)).collect();
-//         Self{
-//             size:src.len(),
-//             data:data
-//         }
-//     }
-// }
-// impl<T:Zero+Add<Output=T>> Matrix for Csr<T> {
-//     type Scalar = T;
-
-//     fn add(&self,other:&Self) -> Self{
-
-//     }
-//     fn sub(&self,other:&Self) -> Self{
-
-//     }
-// }
-
