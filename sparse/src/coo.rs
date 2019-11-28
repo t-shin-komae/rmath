e9a4191f199d49870b0e91e26d3ac335c83fd079
shinichi <t-shin-komae>
2019-11-28 11:51:09 +0900

    implement from_array method to Coo

--- sparse/src/coo.rs
+++ sparse/src/coo.rs
@@ -1,14 +1,14 @@
 use matrix_trait::*;
 use std::ops::{Add,Sub,Mul,Neg};
 
-#[derive(Clone,Hash,PartialEq,Eq)]
+#[derive(Clone,Copy,Hash,PartialEq,Eq)]
 pub struct Coordinate {
     pub i:usize,pub j:usize
 }
 
 pub struct Coo<T> {
-    size:(usize,usize),
-    data:Vec<(Coordinate,T)>
+    pub size:(usize,usize),
+    pub(crate)data:Vec<(Coordinate,T)>
 }
 
 /// data is always sorted
@@ -17,7 +17,7 @@ pub struct CooVec<T> {
     data:Vec<(usize,T)>
 }
 
-impl<T:Zero> CooVec<T> {
+impl<T:Zero+Copy> CooVec<T> {
     pub fn from_vec(src:&[T])->Self{
         Self{
             size:src.len(),
@@ -30,13 +30,20 @@ use crate::dok::Dok;
 use crate::csr::Csr;
 impl<T:Zero+Copy> Coo<T> {
     pub fn from_dok(src:Dok<T>) -> Self {
-        let data_with_ij :Vec<(Coordinate,T)> = IntoIterator::into_iter(src).collect();
+        let mut data_with_ij :Vec<(Coordinate,T)> = src.data.into_iter().collect();
         data_with_ij.sort_unstable_by(|(coo1,_),(coo2,_)|{
             coo1.i.cmp(&coo2.i).then_with(|| coo1.j.cmp(&coo2.j))
         });
         Self{
             size:src.size,
             data:data_with_ij
+        }
+    }
+
+    pub fn from_array(src:&[(usize,usize,T)],size:(usize,usize)) -> Self {
+        Self{
+            size:size,
+            data:src.iter().map(|&(i,j,val)| (Coordinate{i:i,j:j},val)).collect()
         }
     }
 }
